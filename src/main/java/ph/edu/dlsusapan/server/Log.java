3b440bb39b1c4608cb4ddba8f1d7bfc7aceb9bc6
andeng07 <sumilangandreijohn@gmail.com>
2024-04-04 14:02:28 +0800

    feat: added class representation for log

---
+++ src/main/java/ph/edu/dlsusapan/server/Log.java
@@ -0,0 +1,28 @@
+package ph.edu.dlsusapan.server;
+
+import java.util.Date;
+
+public class Log {
+
+    public final Date timestamp;
+    public final String source;
+    public final String destination;
+    public final String action;
+
+    public Log( Date timestamp, String source, String destination, String action) {
+        this.timestamp = timestamp;
+        this.source = source;
+        this.destination = destination;
+        this.action = action;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" +
+                "Timestamp => " + timestamp + "\n" +
+                "Message Source => " + source + "\n" +
+                "Message Destination => " + destination + "\n" +
+                "Action => " + action +
+                "\n".stripIndent();
+    }
+}
